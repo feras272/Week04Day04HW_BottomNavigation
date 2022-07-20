95685ab5aada9e61cf4f7905f815550b69c8f1b8
feras alnowiser <ferasdev@gmail.com>
2022-07-20 12:37:53 +0300

    make the connectViews function and adding id for the frameLayout

--- app/src/main/java/com/example/week04day04hw/MainActivity.kt
+++ app/src/main/java/com/example/week04day04hw/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.week04day04hw
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.Toolbar
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private var toolbar:Toolbar? = null
+    private var frameLayout:FrameLayout? = null
+    private var bottomNavigation:BottomNavigationView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        connectViews()
+    }
+
+    private fun connectViews() {
+        toolbar = findViewById(R.id.toolbar_activity_main)
+        frameLayout = findViewById(R.id.frame_layout_activity_main)
+        bottomNavigation = findViewById(R.id.bottom_nav_activity_main)
     }
 }
