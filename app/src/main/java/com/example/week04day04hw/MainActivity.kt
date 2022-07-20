ef3008fd9b75ba4c59c1619a1b2ced741f2f16b8
feras alnowiser <ferasdev@gmail.com>
2022-07-21 00:08:11 +0300

    adding chooseFragment function which take the fragment the tag and the title of the toolbar as a parameters and adding bottomNavigationClicks function for listening the clicks in each item menu to attach the fragment and the the buttons

--- app/src/main/java/com/example/week04day04hw/MainActivity.kt
+++ app/src/main/java/com/example/week04day04hw/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.week04day04hw
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.FrameLayout
-import android.widget.Toolbar
+import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -16,12 +18,47 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
         connectViews()
+        bottomNavigationClicks()
+        chooseFragment(HomeFragment(), "HOME", "Home")
     }
 
     private fun connectViews() {
         toolbar = findViewById(R.id.toolbar_activity_main)
         frameLayout = findViewById(R.id.frame_layout_activity_main)
         bottomNavigation = findViewById(R.id.bottom_nav_activity_main)
+    }
+
+    private fun chooseFragment(fragment: Fragment, tag:String, title:String) {
+        val fragmentTransaction: FragmentTransaction =supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout_activity_main, fragment, tag)
+        fragmentTransaction.addToBackStack(tag)
+        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+        fragmentTransaction.commit()
+        toolbar?.setTitle(title)
+    }
+
+    private fun bottomNavigationClicks() {
+        bottomNavigation?.setOnItemSelectedListener{
+
+            when(it.itemId) {
+                R.id.home-> {
+                    chooseFragment(HomeFragment(), "HOME", "Home")
+                }
+                R.id.dashboard-> {
+                    chooseFragment(DashboardFragment(), "DASHBOARD", "Dashboard")
+                }
+                R.id.notification -> {
+                    chooseFragment(NotificationFragment(), "NOTIFICATION", "Notification")
+                }
+                R.id.profile-> {
+                    chooseFragment(ProfileFragment(), "PROFILE", "Profile")
+                }
+            }
+            true
+        }
     }
 }
